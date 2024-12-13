d9dd18ec39d34b2d291d5149d4dad76c339367a0
webwithsumit <sumit2410kushwaha@gmail.com>
2024-12-14 00:16:58 +0530

    logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+
+import { Link } from "react-router-dom";
+
+interface Logo {
+  width?: string;
+}
+// Basic Logo
+function Logo({ width = "100px" }: Logo) {
+  return (
+    <Link to={"/"}>
+      <div className={`w-[100px] w-[${width}]`}>
+        <img className="object-cover h-full w-full" src="/logo-light.png" alt="" />
+      </div>
+    </Link>
+  )
+}
+
+export default Logo
