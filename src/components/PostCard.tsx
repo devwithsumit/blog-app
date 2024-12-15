16e9ba338bc51de5cfc8e765d194407865d5ba5e
webwithsumit <sumit2410kushwaha@gmail.com>
2024-12-15 16:31:29 +0530

    Post Card

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+import { Link } from "react-router-dom"
+import fileService from "../appwrite/fileService"
+// import { useEffect } from "react";
+
+
+interface Post {
+    $id?: string;
+    title: string;
+    featuredImage: string;
+}
+
+function PostCard({
+    $id,
+    title,
+    featuredImage
+}: Post) {
+
+    // useEffect(() => {
+    //   const image = fileService.getFilePreview(featuredImage)
+    //   if(image) console.log(image);
+    // }, [])
+    
+    return (
+        <Link to={`/post/${$id}`}>
+            <div className="w-full bg-gray-100 rounded-xl p-4">
+                <div className="w-full justify-center mb-4">
+                    <img src={fileService.getFilePreview(featuredImage)} alt={title}
+                        className="rounded-xl" />
+                </div>
+                <h2 className="text-xl font-bold">
+                    {title}
+                </h2>
+            </div>
+        </Link>
+    )
+}
+
+export default PostCard;
