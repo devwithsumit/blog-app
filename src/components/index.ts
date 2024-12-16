bcbc5f5ce6c36a2d2a210c66dfd48e12608a27de
webwithsumit <sumit2410kushwaha@gmail.com>
2024-12-16 22:43:35 +0530

    index file to export all components together

---
+++ src/components/index.ts
@@ -0,0 +1,27 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Input from "./Header/Input";
+import Logo from "./Logo";
+import Button from "./Header/Button";
+import Login from "./Login";
+import Signup from "./Signup";
+import PostCardPreview from "./PostCard";
+import PostForm from "./post/PostForm";
+import AuthLayout from "./AuthLayout";
+/* export all component together so that there
+is no need to export them in separate lines */
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Button,
+    Input,
+    Login,
+    Signup,
+    PostCardPreview as PostCard,
+    PostForm,
+    AuthLayout,
+}
