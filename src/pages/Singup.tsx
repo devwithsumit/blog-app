84f01d9ed95aa4c42299eda13cdea841959d59d2
webwithsumit <sumit2410kushwaha@gmail.com>
2024-12-18 16:06:12 +0530

    Pages - sign up

---
+++ src/pages/Singup.tsx
@@ -0,0 +1,11 @@
+import { Signup as SingupComponent } from "../components"
+
+function Singup() {
+    return (
+        <div className="py-8">
+            <SingupComponent />
+        </div>
+    )
+}
+
+export default Singup
